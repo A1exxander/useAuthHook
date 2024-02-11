50e86dbe9472d7c123cf7d9f0599cbd19c0e5af6
A1exxander <88210134+A1exxander@users.noreply.github.com>
2024-02-11 11:07:29 -0500

    Add files via upload

---
+++ useAuth.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export default function useAuth() {
+
+  const [JWT, setJWT] = useState(localStorage.getItem("auth")); // Set initial state to use the local storage's auth value
+
+  const updateJWT = (newJWT) => { // Update our JWT by updating local storage, NOT by calling the setState hook!
+    localStorage.setItem("auth", newJWT);
+  };
+
+  useEffect(() => { // Everytime a change occurs to our JWT on local storage, update the state in memory to keep them synchronized
+
+    const handleStorageChange = (event) => { 
+      if (event.key === "auth" ) { // If theyre the same, React wont cause a re-render, so dont worry about checking
+        setJWT(localStorage.getItem("auth"));
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => { window.removeEventListener("storage", handleStorageChange); };
+
+  }, []);
+
+  return [JWT, updateJWT]; // Only allow the user to update JWT via local storage which will then propagate the changes into memory
+
+}
